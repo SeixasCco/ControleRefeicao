eb848cdfc0277203ec69e798247c2d6ae6506348
SeixasCco <raulalves4@gmail.com>
2023-09-13 07:41:34 -0300

    tela geral finalizada

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,6 @@ class HomeController < ApplicationController
   end
   def FAQ
   end
+  def Payment
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "home/FAQ"
 
   get "home/Services"
+
+  get "home/Payment"
 end
