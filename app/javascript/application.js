fe7476c565f24e3da6fcefd1fecb06d199fb6d0e
SeixasCco <raulalves4@gmail.com>
2023-11-12 09:42:55 -0300

    funcionando botões de agua e passos

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
       var height = parseFloat(document.getElementById('height-input').value);
       var weight = parseFloat(document.getElementById('weight-input').value);
       if (height > 0 && weight > 0) {
-        var bmi = (weight / (height * height)).toFixed(2); // BMI = weight(kg) / height(m)^2
+        var bmi = (weight / (height * height)).toFixed(2);
         document.getElementById('bmi-result').innerText = `Seu IMC é: ${bmi}`;
       } else {
         document.getElementById('bmi-result').innerText = 'Por favor, insira valores válidos para altura e peso.';
@@ -17,8 +17,17 @@ function incrementWaterIntake() {
   var progressText = document.getElementById('water-text');
 
   var currentValue = parseInt(progressBar.value);
-  currentValue = (currentValue >= 8) ? 0 : currentValue + 1;
+  currentValue = (currentValue >= 10) ? 0 : currentValue + 1;
 
   progressBar.value = currentValue;
   progressText.textContent = currentValue + " de " + progressBar.max + " copos";
+}
+
+function incrementStepCount() {
+  var stepCountParagraph = document.getElementById('step-count');
+  var currentStepCount = parseInt(stepCountParagraph.textContent.replace(/\D/g,''));
+
+  var newStepCount = (currentStepCount + 100) % 5000;
+
+  stepCountParagraph.textContent = newStepCount.toLocaleString() + " passos";
 }
